5bcc7ed683175f22339d6d54436a25ed3e1a1d9b
Fuggel <furkanserdarceylan@gmail.com>
2024-08-23 20:54:47 +0200

    fix: use permission status in dep array

--- src/hooks/useUserLocation.ts
+++ src/hooks/useUserLocation.ts
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 export default function useUserLocation() {
+    const [permissionStatus, setPermissionStatus] = useState<Location.PermissionStatus | null>(null);
     const [userLocation, setUserLocation] = useState<Location.LocationObject | null>(null);
 
     const checkLocationPermission = async () => {
         const { status } = await Location.requestForegroundPermissionsAsync();
-        const location = await Location.getCurrentPositionAsync({});
+        setPermissionStatus(status);
 
         if (status === Location.PermissionStatus.GRANTED) {
+            const location = await Location.getCurrentPositionAsync({});
             setUserLocation(location);
         } else {
             setUserLocation(null);
@@ -17,7 +19,7 @@ export default function useUserLocation() {
 
     useEffect(() => {
         checkLocationPermission();
-    }, [userLocation]);
+    }, [permissionStatus]);
 
     return { userLocation };
 }
