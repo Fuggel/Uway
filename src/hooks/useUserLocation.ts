7dae0e4096092326787f60e66865ac0406447e0c
Fuggel <furkanserdarceylan@gmail.com>
2024-08-25 16:23:52 +0200

    fix(geolocation): watch user location and higher distance interval to reduce jumps

--- src/hooks/useUserLocation.ts
+++ src/hooks/useUserLocation.ts
@@ -2,24 +2,38 @@ import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 export default function useUserLocation() {
-    const [permissionStatus, setPermissionStatus] = useState<Location.PermissionStatus | null>(null);
     const [userLocation, setUserLocation] = useState<Location.LocationObject | null>(null);
 
-    const checkLocationPermission = async () => {
-        const { status } = await Location.requestForegroundPermissionsAsync();
-        setPermissionStatus(status);
-
-        if (status === Location.PermissionStatus.GRANTED) {
-            const location = await Location.getCurrentPositionAsync({});
-            setUserLocation(location);
-        } else {
-            setUserLocation(null);
-        }
-    };
-
     useEffect(() => {
+        let locationSubscription: Location.LocationSubscription | null = null;
+
+        const checkLocationPermission = async () => {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status === Location.PermissionStatus.GRANTED) {
+                locationSubscription = await Location.watchPositionAsync(
+                    {
+                        accuracy: Location.Accuracy.BestForNavigation,
+                        timeInterval: 1000,
+                        distanceInterval: 5,
+                    },
+                    (location) => {
+                        setUserLocation(location);
+                    }
+                );
+            } else {
+                setUserLocation(null);
+            }
+        };
+
         checkLocationPermission();
-    }, [permissionStatus]);
+
+        return () => {
+            if (locationSubscription) {
+                locationSubscription.remove();
+            }
+        };
+    }, []);
 
     return { userLocation };
 }
