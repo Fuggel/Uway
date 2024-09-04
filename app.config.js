3b0e564392146dc0832d20d4213fb8fb316d35e7
Fuggel <furkanserdarceylan@gmail.com>
2024-09-04 23:13:51 +0200

    chore: bump to 1.1.6

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => {
         ...config,
         name: "NavSync",
         slug: "NavSync",
-        version: "1.1.5",
+        version: "1.1.6",
         orientation: "portrait",
         icon: "./src/assets/images/build/icon.png",
         scheme: "myapp",
@@ -20,7 +20,7 @@ export default ({ config }) => {
             backgroundColor: "#ffffff"
         },
         ios: {
-            buildNumber: "1.1.5",
+            buildNumber: "1.1.6",
             supportsTablet: true,
             bundleIdentifier: "com.fuggel.NavSync"
         },
