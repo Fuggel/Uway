e57e75a19ee5939862c6409654d346bcef8b6388
Fuggel <furkanserdarceylan@gmail.com>
2024-11-28 21:10:52 +0100

    feat: play micro sounds

--- src/hooks/useSpeechToText.ts
+++ src/hooks/useSpeechToText.ts
@@ -1,18 +1,41 @@
+import { Audio } from "expo-av";
 import { useEffect, useRef, useState } from "react";
-import { LogBox } from "react-native";
+import { LogBox, Platform } from "react-native";
 
 import Voice from "@react-native-voice/voice";
 
 LogBox.ignoreLogs([`new NativeEventEmitter()`]);
+
+const SOUND_FILES = {
+    mic_on: require("../assets/sounds/microphone-on.mp3"),
+    mic_off: require("../assets/sounds/microphone-off.mp3"),
+};
 
 const useSpeechToText = () => {
     const [text, setText] = useState<string | null>(null);
     const [isListening, setIsListening] = useState(false);
     const silenceTimeout = useRef<NodeJS.Timeout | null>(null);
 
+    const playSound = async (key: keyof typeof SOUND_FILES) => {
+        if (Platform.OS !== "ios") return;
+
+        try {
+            const { sound } = await Audio.Sound.createAsync(SOUND_FILES[key]);
+            await sound.playAsync();
+            sound.setOnPlaybackStatusUpdate((status) => {
+                if (!status.isLoaded) {
+                    sound.unloadAsync();
+                }
+            });
+        } catch (error) {
+            console.log(`Error loading or playing sound: ${error}`);
+        }
+    };
+
     const startListening = async () => {
         try {
             setText(null);
+            await playSound("mic_on");
             await Voice.start("de-DE");
             setIsListening(true);
         } catch (error) {
@@ -22,6 +45,7 @@ const useSpeechToText = () => {
 
     const stopListening = async () => {
         try {
+            await playSound("mic_off");
             await Voice.stop();
             setIsListening(false);
             clearTimeout(silenceTimeout.current!);
