2b554640a81422256d51fd15249e45b677516b6d
Fuggel <furkanserdarceylan@gmail.com>
2024-12-26 14:13:45 +0100

    feat: fitbounds for waypoints

--- src/hooks/useMapCamera.ts
+++ src/hooks/useMapCamera.ts
@@ -3,11 +3,13 @@ import { Dimensions } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 import { CameraRef } from "@rnmapbox/maps/lib/typescript/src/components/Camera";
-import { Position } from "@turf/helpers";
+import { Geometry, Position } from "@turf/helpers";
 
 import { MAP_CONFIG } from "@/constants/map-constants";
+import { MapFeatureContext } from "@/contexts/MapFeatureContext";
 import { UserLocationContext } from "@/contexts/UserLocationContext";
 import { mapNavigationActions, mapNavigationSelectors } from "@/store/mapNavigation";
+import { mapWaypointSelectors } from "@/store/mapWaypoint";
 import { convertSpeedToKmh } from "@/utils/map-utils";
 
 const { width, height } = Dimensions.get("window");
@@ -16,11 +18,14 @@ const useMapCamera = () => {
     const cameraRef = useRef<CameraRef | null>(null);
     const dispatch = useDispatch();
     const { userLocation } = useContext(UserLocationContext);
+    const { gasStations } = useContext(MapFeatureContext);
     const directions = useSelector(mapNavigationSelectors.directions);
     const tracking = useSelector(mapNavigationSelectors.tracking);
     const isNavigationSelecting = useSelector(mapNavigationSelectors.isNavigationSelecting);
     const navigationView = useSelector(mapNavigationSelectors.navigationView);
     const navigationMode = useSelector(mapNavigationSelectors.isNavigationMode);
+    const isGasStationWaypoint = useSelector(mapWaypointSelectors.gasStationWaypoints);
+    const selectingGasStationWaypoint = useSelector(mapWaypointSelectors.selectGasStationWaypoint);
 
     const userSpeed = userLocation?.coords.speed || 0;
     const currentSpeed = convertSpeedToKmh(userSpeed);
@@ -51,11 +56,51 @@ const useMapCamera = () => {
         );
     };
 
+    const fitBoundsToUserAndGasStations = () => {
+        if (!cameraRef.current || !userLocation || !gasStations?.gasStations?.features) return;
+
+        const userCoords: Position = [userLocation.coords.longitude, userLocation.coords.latitude];
+
+        const gasStationCoords = gasStations.gasStations.features.map((gasStation) => {
+            const geometry = gasStation.geometry as Geometry;
+            return geometry.coordinates;
+        }) as Position[];
+
+        const allCoords: Position[] = [userCoords, ...gasStationCoords];
+
+        const lons = allCoords.map(([lon]) => lon);
+        const lats = allCoords.map(([, lat]) => lat);
+
+        const ne: Position = [Math.max(...lons), Math.max(...lats)];
+        const sw: Position = [Math.min(...lons), Math.min(...lats)];
+
+        const WAYPOINT_OFFSET_HORIZONTAL = 0.15;
+        const WAYPOINT_OFFSET_BOTTOM = 0.35;
+
+        cameraRef.current.fitBounds(
+            ne,
+            sw,
+            [
+                0,
+                width * MAP_CONFIG.boundsOffset * WAYPOINT_OFFSET_HORIZONTAL,
+                height * WAYPOINT_OFFSET_BOTTOM,
+                width * MAP_CONFIG.boundsOffset * WAYPOINT_OFFSET_HORIZONTAL,
+            ],
+            MAP_CONFIG.animationDuration
+        );
+    };
+
     useEffect(() => {
-        if (isNavigationSelecting && directions && tracking) {
+        if (isNavigationSelecting && directions && tracking && !selectingGasStationWaypoint) {
             fitBounds();
         }
-    }, [isNavigationSelecting, directions, tracking]);
+    }, [isNavigationSelecting, directions, tracking, isGasStationWaypoint, selectingGasStationWaypoint]);
+
+    useEffect(() => {
+        if (directions && selectingGasStationWaypoint && tracking) {
+            fitBoundsToUserAndGasStations();
+        }
+    }, [directions, selectingGasStationWaypoint, tracking]);
 
     useEffect(() => {
         const updateCamera = () => {
