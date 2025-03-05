07a48d25789ff11a16dd497ef906e0aee276fe73
Fuggel <furkanserdarceylan@gmail.com>
2025-03-05 22:39:17 +0100

    chore: bump to 0.2.1

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
         owner: "fuggel",
         name: "Uway",
         slug: "uway",
-        version: "0.2.0",
+        version: "0.2.1",
         orientation: "portrait",
         icon: "./src/assets/images/build/icon.png",
         scheme: "Uway",
@@ -21,7 +21,7 @@ export default ({ config }) => {
             backgroundColor: "#ffffff",
         },
         ios: {
-            buildNumber: "0.2.0",
+            buildNumber: "0.2.1",
             supportsTablet: true,
             bundleIdentifier: "com.fuggel.Uway",
             infoPlist: {
