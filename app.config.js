8f671672e27324e89e8d548b00397d0bf8ef8001
Fuggel <furkanserdarceylan@gmail.com>
2025-03-06 18:34:47 +0100

    fix: remove latest image selection for ios

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
         owner: "fuggel",
         name: "Uway",
         slug: "uway",
-        version: "0.2.1",
+        version: "0.2.2",
         orientation: "portrait",
         icon: "./src/assets/images/build/icon.png",
         scheme: "Uway",
@@ -21,7 +21,7 @@ export default ({ config }) => {
             backgroundColor: "#ffffff",
         },
         ios: {
-            buildNumber: "0.2.1",
+            buildNumber: "0.2.2",
             supportsTablet: true,
             bundleIdentifier: "com.fuggel.Uway",
             infoPlist: {
